54242d0d7b28cad5199718e729fc05d7aeb724d3
cngondo <devngondo@gmail.com>
2016-09-06 16:38:21 +0300

    sign in prompt for new user working

--- app/src/main/java/firebase/example/com/fairefb/MainActivity.java
+++ app/src/main/java/firebase/example/com/fairefb/MainActivity.java
@@ -1,15 +1,31 @@
 package firebase.example.com.fairefb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity{
+
+    private static final String TAG = "AUTHENTICATED";
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +34,64 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //check authentication and redirect to loginActivity if the user does not exist
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Toast.makeText(MainActivity.this, "Signed in as " + user.getUid(), Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Sorry!! You need to sign in first", Toast.LENGTH_LONG).show();
+                    loadLoginView();
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    /*
+    * The following method navigates to the Login view and clears the activity stack. This prevents the user going
+    * back to the main activity when they press the Back button from the login view.
+    * */
+    private void loadLoginView(){
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        //created the menu options
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return  true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        handle click events on the menu
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
