d19cd6b3dfe84a3e155c40fe205cfa3b404238cb
cngondo <devngondo@gmail.com>
2016-09-06 11:08:02 +0300

    created additional activities and added menu item for logout

---
+++ app/src/main/java/firebase/example/com/fairefb/Constants.java
@@ -0,0 +1,9 @@
+package firebase.example.com.fairefb;
+
+/**
+ * Created by ngondo on 9/6/16.
+ */
+
+public class Constants {
+    public static final String FIREBASE_URL = "https://fairefb-1728b.firebaseio.com";
+}
---
+++ app/src/main/java/firebase/example/com/fairefb/MainActivity.java
@@ -0,0 +1,28 @@
+package firebase.example.com.fairefb;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
